da518504e5ff9416f8f50683c0bb3a0ebd7de1fd
sdschultzie <33032954+sdschultzie@users.noreply.github.com>
2025-11-05 08:33:50 +0000

    setup vscode dev containers for both api and web apps. Hot module reload inside the web docker container

--- web/pokemon-comparison/vite.config.ts
+++ web/pokemon-comparison/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true
+    },
+    host: true,
+    strictPort: true,
+    port: 1112, // Need to explicitly set port to something other than the default 5173 for HMR to work
+  },
 })
